db90becbeef274002fac33a9110f94f854c510ed
MATS5637 <matheussuliano380@gmail.com>
2025-09-03 10:49:16 -0300

    mudança da ativação da caixa de texto

--- projeto-ipc/frontend/frontend.py
+++ projeto-ipc/frontend/frontend.py
@@ -6,27 +6,20 @@ root.title('Jorge')
 topbar = Frame(root)
 topbar.pack(padx=12, pady=12)
 
-area = Frame(root)
-area.pack(fill='x', padx=12, pady=(0, 12))
+def myClick():
+    Label(text="Mensagem:").pack()
+    e = Entry(width=50)
+    e.pack()    
 
-Label(area, text="Mensagem:").pack(anchor='w')
-e = Entry(area, width=50)
-e.pack(anchor='w', pady=4)
+    def mySecondClick():
+        resposta =e.get()
+        mySecondLabel = Label(root, text=resposta)
+        mySecondLabel.pack()
 
-resp_var = StringVar(value="")
-Label(root, textvariable=resp_var).pack(pady=(4, 10))
+    Button(text="Enviar", command=mySecondClick).pack(anchor='w', pady=4)
 
-def enviar():
-    resp_var.set(e.get())
-
-Button(area, text="Enviar", command=enviar).pack(anchor='w', pady=4)
-
-def myClick(tipo):
-    
-    e.insert(0, f"{tipo}: ")
-
-Button(topbar, text="pipes", command=lambda: myClick("pipes")).pack(side='left', padx=8)
-Button(topbar, text="Sockets", command=lambda: myClick("Sockets")).pack(side='left', padx=8)
-Button(topbar, text="Memória compartilhada", command=lambda: myClick("Memória compartilhada")).pack(side='left', padx=8)
+Button(topbar, text="pipes", command = myClick).pack(side='left', padx=8)
+Button(topbar, text="Sockets", command = myClick).pack(side='left', padx=8)
+Button(topbar, text="Memória compartilhada", command = myClick).pack(side='left', padx=8)
 
 root.mainloop()
